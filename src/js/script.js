2e3c864770bc6b204b17dfd27a35184992e8efc5
Kacper Kosicki <kosickikacper1@gmail.com>
2023-07-12 20:49:16 +0200

    Finish project

--- src/js/script.js
+++ src/js/script.js
@@ -17,14 +17,42 @@ const app = {
   renderProducts: function () {
     const productsTemplate = document.getElementById('product-template').innerHTML;
     const compiledTemplate = Handlebars.compile(productsTemplate);
-    const renderedHTML = compiledTemplate(this.data.products);
+    const renderedHTML = compiledTemplate({ 
+      products: [
+        {
+          image: '/images/coffee-1.png',
+          title: '01. LA LAGARTIJA',
+          description: 'Mysterious and treacherous... The best choice for the start of your journey, it will provide you a rich delicious flavor.',
+          mostpopular: '/images/mostpopular.png',
+          roasting: '5/10',
+          intensity: '6/10',
+          imagePositionLeft: false
+        },
+        {
+          image: '/images/coffee-2.png',
+          title: '02. EL TIGRE',
+          description: 'Really dark coffee, only for the real gourmets. It has a bitter flavor of grapefruits and is mixed with a little bit of peanuts with caramel.',
+          roasting: '8/10',
+          intensity: '9/10',
+          imagePositionLeft: true
+        },
+        {
+          image: '/images/coffee-3.png',
+          title: '03. LA CUARACHA',
+          description: 'Sweet and intensive with a big amount of different flavors. Caramel, grapes, strawberry, you name it! It\'s a great choice for warm summer days.',
+          roasting: '3/10',
+          intensity: '7/10',
+          imagePositionLeft: false
+        }
+      ]
+    });
     const productsContainer = document.getElementById('products-container');
     productsContainer.innerHTML = renderedHTML;
   },
 
   init: function () {
-    const thisApp = this;
-    thisApp.initData();
+    this.initData();
+    this.renderProducts();
   },
 };
 
